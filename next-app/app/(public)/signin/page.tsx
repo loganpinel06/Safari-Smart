d8646f300ab71d891b93d10382ff330183d3d0ba
Logan Pinel <loganpinel@outlook.com>
2026-02-26 16:31:06 -0500

    Updated requireAuth wrapper to send a message to frontend through query parameters, and added functionality to recieve and render the message if it exists in page.tsx

--- next-app/app/(public)/signin/page.tsx
+++ next-app/app/(public)/signin/page.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import { useState } from "react";
+//import useSearchParams hook for getting query parameters from the URL
+import { useSearchParams } from "next/navigation";
+
 //Variables
 export default function Page() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  //get the message query parameter from the URL if it exists
+  const searchParams = useSearchParams();
+  const message = searchParams.get("message");
+
   return (
     <div>
+      {/* render the message if it exists */}
+      {message && <p>{message}</p>}
       <p>Enter your details.</p>
       <input
         type="email"
