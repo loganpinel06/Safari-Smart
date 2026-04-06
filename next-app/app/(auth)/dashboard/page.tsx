e35eecb6bb804c9d05c6e978fefc27566887fbb4
megandowdell <megan@dowdellfamily.com>
2026-04-06 16:37:47 -0400

    adding join class section

--- next-app/app/(auth)/dashboard/page.tsx
+++ next-app/app/(auth)/dashboard/page.tsx
@@ -7,6 +7,7 @@ import { redirect } from "next/navigation";
 import { getSubjects } from "@/utils/categories/util";
 import TeacherDashboardContent from "@/components/TeacherDashboardContent";
 import ParentDashboardContent from "@/components/ParentDashboardContent";
+import JoinClassSection from "@/components/JoinClassSection";
 
 export default async function DashboardPage() {
   const supabase = await createClient();
